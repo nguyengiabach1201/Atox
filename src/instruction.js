a6dd5f9f8017d80462edd8e04b4a3e02f5d80aa8
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-22 19:58:38 +0700

    Update instruction.js

--- src/instruction.js
+++ src/instruction.js
@@ -41,6 +41,36 @@ export class Instruction {
 export class Disassembler {
   constructor(src) {
     this.src = src;
-    
+    this.instructions = [];
+  }
+
+  error(msg, line) {
+    console.error(`Error: ${msg} at line ${line}`);
+  }
+
+  disassemble() {
+    const lines = this.src.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line === "") continue;
+      const parts = line.split(" ");
+      const instruction = parts[0];
+      const args = parts.slice(1);
+
+      if (!(instruction in instructions))
+        this.error(`Unknown instruction: ${instruction}`, i + 1);
+
+      args.forEach((arg) => {
+        if (!(arg in registers) && typeof parseInt(arg) !== NaN) {
+          this.error(
+            `Invalid argument, argument(s) must be registers or interger`,
+            i + 1,
+          );
+          continue;
+        }
+      });
+
+      this.instructions.push(new Instruction(instruction, args));
+    }
   }
 }
