6994b425905d5fd8be93e9ee13fc5b07e03c352d
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-22 17:55:18 +0700

    Update instruction.js

--- src/instruction.js
+++ src/instruction.js
@@ -1,1 +1,33 @@
+export const instructions = {
+  mov: "mov",
+  add: "add",
+  sub: "sub",
+  mul: "mul",
+  div: "div",
+  and: "and",
+  or: "or",
+  xor: "xor",
+  not: "not",
+  jmp: "jmp", // go to a if b
+  sec: "sec", // section
+};
+
+export const registers = {
+  v0: "v0",
+  v1: "v1",
+  v2: "v2",
+  v3: "v3",
+  v4: "v4",
+  v5: "v5",
+  v6: "v6",
+  v7: "v7",
+  v8: "v8",
+  v9: "v9",
+  vA: "vA",
+  vB: "vB",
+  vC: "vC",
+  vD: "vD",
+  vE: "vE",
+  vF: "vF",
+};
 
