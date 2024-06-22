a696497550f549dc783b3b2c1f5e4ae061400018
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-22 09:38:08 +0700

    Create cpu.js

---
+++ src/cpu.js
@@ -0,0 +1,13 @@
+class CPU {
+  constructor() {
+    this.memory = new Uint8Array(4096);
+    this.registers = new Uint8Array(16);
+    this.stack = new Uint16Array(16);
+    
+    this.ST = 0;
+    this.DT = 0;
+    this.I = 0;
+    this.SP = -1;
+    this.PC = 0x200;
+  }
+}
