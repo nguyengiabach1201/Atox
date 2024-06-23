b6e88d05a50da37ab5ef176dd0cd74f865f5fc04
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-23 17:04:38 +0700

    Update cpu.js

--- src/cpu.js
+++ src/cpu.js
@@ -1,18 +1,69 @@
+export class Rom {
+  constructor(src) {
+    this.src = src;
+    this.data = this.compile(sr);
+  }
+
+  compile(src) {
+    let data = "";
+    for (let i = 0; i < src.length; i++) {
+      switch (src[i]) {
+        case ">":
+          data += ">";
+          break;
+        case "<":
+          data += "<";
+          break;
+        case "+":
+          data += "+";
+          break;
+        case "-":
+          data += "-";
+          break;
+        case ".":
+          data += ".";
+          break;
+        case ",":
+          data += ",";
+          break;
+        case "[":
+          data += "[";
+          break;
+        case "]":
+          data += "]";
+          break;
+      }
+    }
+    return data;
+  }
+}
+
 export class CPU {
   constructor() {
     this.reset();
   }
   reset() {
     this.memory = new Uint8Array(1024);
-    this.PC = 0;
+
+    this.PC = 0x200;
+    this.ptr = 0;
   }
   load(rom) {
     this.reset();
-    for (let i = 0; i < rom.length; i++) this.memory[this.PC + i] = rom[i];
+    for (let i = 0; i < rom.length; i++) this.memory[i] = rom[i];
   }
   fetch() {
     if (this.PC > 1024) throw new Error("Memory out of bounds.");
     return this.memory[this.PC];
+  }
+  run() {
+    while (1) {
+      try {
+        this.step();
+      } catch {
+        break;
+      }
+    }
   }
   step() {
     const op = this.fetch();
@@ -21,5 +72,53 @@ export class CPU {
   execute(op) {
     op = op.toString(16);
     console.log(op);
+
+    switch (op) {
+      case ">":
+        this.ptr++;
+        break;
+      case "<":
+        this.ptr--;
+        break;
+      case "+":
+        this.memory[this.ptrptr]++;
+        break;
+      case "-":
+        this.memory[this.ptrptr]--;
+        break;
+      case ".":
+        console.log(String.fromCharCode(memory[this.ptrptr]));
+        break;
+      case ",":
+        this.memory[this.ptrptr] = 0;
+        break;
+      case "[":
+        if (this.memory[this.ptrptr] === 0) {
+          i++;
+          while (code[i] !== "]" || code[i - 1] === "[") {
+            if (code[i] === "[") {
+              i++;
+            } else if (code[i] === "]") {
+              i--;
+            }
+            i++;
+          }
+        }
+        break;
+      case "]":
+        if (this.memory[this.ptr] !== 0) {
+          i--;
+          while (code[i] !== "[" || code[i - 1] === "]") {
+            if (code[i] === "]") {
+              i--;
+            } else if (code[i] === "[") {
+              i++;
+            }
+            i--;
+          }
+          i--;
+        }
+        break;
+    }
   }
 }
