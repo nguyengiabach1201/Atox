07078361f5aeb5c24e1a6e0c29cb2764a9f09c6d
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-23 17:19:56 +0700

    Update cpu.js

--- src/cpu.js
+++ src/cpu.js
@@ -1,40 +1,9 @@
 export class Rom {
   constructor(src) {
-    this.src = src;
-    this.data = this.compile(sr);
-  }
-
-  compile(src) {
-    let data = "";
-    for (let i = 0; i < src.length; i++) {
-      switch (src[i]) {
-        case ">":
-          data += ">";
-          break;
-        case "<":
-          data += "<";
-          break;
-        case "+":
-          data += "+";
-          break;
-        case "-":
-          data += "-";
-          break;
-        case ".":
-          data += ".";
-          break;
-        case ",":
-          data += ",";
-          break;
-        case "[":
-          data += "[";
-          break;
-        case "]":
-          data += "]";
-          break;
-      }
-    }
-    return data;
+    this.data = src
+      .split("")
+      .filter((char) => [">", "<", "+", "-", ".", ",", "[", "]"].includes(char))
+      .join("");
   }
 }
 
@@ -43,82 +12,69 @@ export class CPU {
     this.reset();
   }
   reset() {
-    this.memory = new Uint8Array(1024);
-
-    this.PC = 0x200;
+    this.memory = new Array(1024);
     this.ptr = 0;
   }
   load(rom) {
     this.reset();
-    for (let i = 0; i < rom.length; i++) this.memory[i] = rom[i];
-  }
-  fetch() {
-    if (this.PC > 1024) throw new Error("Memory out of bounds.");
-    return this.memory[this.PC];
-  }
-  run() {
-    while (1) {
-      try {
-        this.step();
-      } catch {
-        break;
-      }
+    
+    for (let i = 0; i < rom.data.length; i++) {
+      // console.log(rom.data[i]);
+      this.memory[i] = rom.data[i];
     }
+    // this.execute();
+    // console.log(this.memory[0]);
   }
-  step() {
-    const op = this.fetch();
-    this.execute(op);
-  }
-  execute(op) {
-    op = op.toString(16);
-    console.log(op);
-
-    switch (op) {
-      case ">":
-        this.ptr++;
-        break;
-      case "<":
-        this.ptr--;
-        break;
-      case "+":
-        this.memory[this.ptrptr]++;
-        break;
-      case "-":
-        this.memory[this.ptrptr]--;
-        break;
-      case ".":
-        console.log(String.fromCharCode(memory[this.ptrptr]));
-        break;
-      case ",":
-        this.memory[this.ptrptr] = 0;
-        break;
-      case "[":
-        if (this.memory[this.ptrptr] === 0) {
-          i++;
-          while (code[i] !== "]" || code[i - 1] === "[") {
-            if (code[i] === "[") {
-              i++;
-            } else if (code[i] === "]") {
-              i--;
-            }
+  execute() {
+    // console.log(this.memory);
+    for (let i = 0; i < this.memory.length; i++) {
+      switch (this.memory[i]) {
+        case ">":
+          this.ptr++;
+          break;
+        case "<":
+          this.ptr--;
+          break;
+        case "+":
+          this.memory[this.ptr]++;
+          break;
+        case "-":
+          this.memory[this.ptr]--;
+          break;
+        case ".":
+          console.log(String.fromCharCode(this.memory[this.ptr]));
+          break;
+        case ",":
+          this.memory[this.ptr] = 0;
+          break;
+        case "[":
+          if (this.memory[this.ptr] === 0) {
             i++;
-          }
-        }
-        break;
-      case "]":
-        if (this.memory[this.ptr] !== 0) {
-          i--;
-          while (code[i] !== "[" || code[i - 1] === "]") {
-            if (code[i] === "]") {
-              i--;
-            } else if (code[i] === "[") {
+            while (this.memory[i] !== "]" || this.memory[i - 1] === "[") {
+              if (this.memory[i] === "[") {
+                i++;
+              } else if (this.memory[i] === "]") {
+                i--;
+              }
               i++;
+            }
+          }
+          break;
+        case "]":
+          if (this.memory[this.ptr] !== 0) {
+            i--;
+            while (this.memory[i] !== "[" || this.memory[i - 1] === "]") {
+              if (this.memory[i] === "]") {
+                i--;
+              } else if (this.memory[i] === "[") {
+                i++;
+              }
+              i--;
             }
             i--;
           }
-          i--;
-        }
-        break;
+          break;
+      }
     }
   }
 }
