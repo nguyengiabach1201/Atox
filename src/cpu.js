ebc35efcf7ed4feea8cb513a4be607b5bdbec8a0
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-23 16:48:29 +0700

    Update cpu.js

--- src/cpu.js
+++ src/cpu.js
@@ -3,22 +3,12 @@ export class CPU {
     this.reset();
   }
   reset() {
-    this.memory = new Uint8Array(4096);
-    this.registers = new Uint8Array(16);
-    this.stack = new Uint16Array(16);
-
-    this.ST = 0;
-    this.DT = 0;
-    this.I = 0;
-    this.SP = -1;
+    this.memory = new Uint8Array(1024);
     this.PC = 0x200;
   }
   load(rom) {
     this.reset();
-    for (let i = 0; i < rom.length; i++) {
-      this.memory[this.PC + 2 * i] = rom[i] >> 8;
-      this.memory[this.PC + 2 * i + 1] = rom[i] & 0x00ff;
-    }
+    for (let i = 0; i < rom.length; i++) this.memory[this.PC + i] = rom[i];
   }
   fetch() {
     if (this.PC > 4095) throw new Error("Memory out of bounds.");
