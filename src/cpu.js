b3099ba3ff49f60f66869e36586c968f5325d807
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-23 10:15:25 +0700

    Update cpu.js

--- src/cpu.js
+++ src/cpu.js
@@ -1,7 +1,5 @@
-import { Disassembler } from "./instruction.js";
-
-class CPU {
-  constructor(src) {
+export class CPU {
+  constructor() {
     this.memory = new Uint8Array(4096);
     this.registers = new Uint8Array(16);
     this.stack = new Uint16Array(16);
@@ -11,48 +9,5 @@ class CPU {
     this.I = 0;
     this.SP = -1;
     this.PC = 0x200;
-
-    this.disassembler = new Disassembler(src);
-    this.disassembler.disassemble();
-    this.instructions = this.disassembler.instructions;
-  }
-
-  run() {
-    
-  }
-
-  execute(instruction) {
-    const id = instruction.instruction.id;
-    const args = instruction.args;
-
-    switch (id) {
-      case "mov": {
-        const reg = args[0],
-          val = args[1];
-
-        if (val in registers) this.registers[reg] = this.registers[val];
-        else this.registers[reg] = parseInt(val);
-
-        break;
-      }
-      case "add": {
-        const reg = args[0],
-          val = args[1];
-
-        if (val in registers) this.registers[reg] += this.registers[val];
-        else this.registers[reg] += parseInt(val);
-
-        break;
-      }
-      case "sub": {
-        const reg = args[0],
-          val = args[1];
-
-        if (val in registers) this.registers[reg] -= this.registers[val];
-        else this.registers[reg] -= parseInt(val);
-
-        break;
-      }
-    }
   }
 }
